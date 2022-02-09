fab879024ae363e8dc198235c7c1d742f96367a9
Batu-Pro16 <david77115@gmail.com>
2022-02-09 10:03:30 +0800

    feat: setup layout

--- app/src/main/java/com/dcard/koinsample/MainActivity.kt
+++ app/src/main/java/com/dcard/koinsample/MainActivity.kt
@@ -2,10 +2,38 @@ package com.dcard.koinsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dcard.koinsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setupViews()
+    }
+
+    private fun setupViews(){
+
+        binding.apply {
+            depositButton.setOnClickListener {
+                cleanInput()
+            }
+
+            withdrawButton.setOnClickListener {
+                cleanInput()
+            }
+
+            remitButton.setOnClickListener {
+                cleanInput()
+            }
+        }
+    }
+
+    private fun cleanInput(){
+        binding.editText.text = null
     }
 }
