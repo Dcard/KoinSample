7e34a4b597240371678ce1060f31ccc19411204d
Batu-Pro16 <david77115@gmail.com>
2022-02-10 10:14:59 +0800

    feat: get DI instance by koin directly from ViewModel

--- app/src/main/java/com/dcard/koinsample/MainViewModel.kt
+++ app/src/main/java/com/dcard/koinsample/MainViewModel.kt
@@ -3,19 +3,28 @@ package com.dcard.koinsample
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.dcard.component.AtmProvider
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.parameter.parametersOf
 
 /**
  * @author Batu
  */
 class MainViewModel(
-    private val atm: AtmProvider
-) : ViewModel() {
+    private val useCase: UseCase
+) : ViewModel(), KoinComponent {
+
+    private val atm: AtmProvider by inject {
+        parametersOf(useCase)
+    }
 
     val amount: LiveData<Int>
         get() = atm.amount
 
     fun onDeposit(value: Int) {
         atm.onDeposit(value)
+        // Another way to get AtmProvider instance by Koin
+        /* get<AtmProvider> { parametersOf(useCase) }.onDeposit(value) */
     }
 
     fun onWithdraw(value: Int) {
